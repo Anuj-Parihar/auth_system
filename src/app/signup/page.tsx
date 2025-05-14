d7d8c45e5d52dfffc6b7ce535b278a8f2d447ade
Anuj-Parihar <anujparihar0001@gmail.com>
2025-05-15 00:25:51 +0530

    manage params

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { axios } from "axios";
+// import { axios } from "axios";
 
 export default function SignupPage() {
   const [user, setUser] = React.useState({
