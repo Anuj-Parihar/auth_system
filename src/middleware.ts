9dbc2652c316288358f78fc2a407ea59bf3c8ff9
Anuj-Parihar <anujparihar0001@gmail.com>
2025-05-21 00:40:03 +0530

    add middleware and create route for me(meUser)

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+// login path
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/login" || path === "/signup";
+
+  const token = request.cookies.get("token")?.value || "";
+
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL('/',request.nextUrl))
+    }
+
+    if (!isPublicPath && !token) {
+        return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
+
+//   const isProtectedPath = path === "/profile" || path === "/";
+}
+//see "Matching Paths" below to learn more about how to match paths
+
+//matching paths
+export const config = {
+  matcher: ["/", "/login", "/profile", "/signup"],
+};
